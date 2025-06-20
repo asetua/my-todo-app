99369744a4960c0c9746e68dcfd67396ea7c376a
Oluseyi Oguntimehin <oguntimane@gmail.com>
2025-06-20 12:04:43 +0100

    Update index js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.post("/api/todos", async (req, res) => {
   const { title } = req.body;
   const newTodo = new Todo({ title });
   await newTodo.save();
-  res.status(201).json(newTodo);
+  res.status(201).json(savedTodo);
 });
 
 app.delete("/api/todos/:id", async (req, res) => {
